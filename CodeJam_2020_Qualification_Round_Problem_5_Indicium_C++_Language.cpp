6c5bfcea94f06db3bb929fbb5cd0d42101a35c79
Shubh Patel <61275950+Shubh-Official@users.noreply.github.com>
2020-04-05 19:55:00 +0530

    Update CodeJam_2020_Qualification_Round_Problem_5_Indicium_C++_Language.cpp

--- CodeJam_2020_Qualification_Round_Problem_5_Indicium_C++_Language.cpp
+++ CodeJam_2020_Qualification_Round_Problem_5_Indicium_C++_Language.cpp
@@ -1,61 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
 
-int square[50][50], n, k, i;
-bool rowSafe[50][50], columnSafe[50][50], flag;
+const int N = 15;
 
-void evaluate(int row, int column, int m)
+int a[N][N];
+int pos[N];
+int flag;
+
+void rec(int i, int j, int n, int sum)
 {
-    if (row == n && column == (n+1) && m == k && !flag)
-    {
-        flag = true;
-        cout << "Case #" << (i+1) << ": " << "POSSIBLE\n";
-        for (int j=1;j<=n;j++)
-        {
-            for (int z=1;z<=n;z++)
-            {
-                cout << square[j][z] << " ";
-            }
-            cout << endl;
-        }
-        return;
-    }
-    else if (row > n)
-        return;
-    else if (column > n)
-        evaluate(row + 1, 1, m);
-    for (int j = 1; j <= n && !flag; j++)
-    {
-        if (!rowSafe[row][j] && !columnSafe[column][j])
-        {
-            rowSafe[row][j] = true;
-            columnSafe[column][j] = true;
-            if (row == column)
-                m += j;
-
-            square[row][column] = j;
-            evaluate(row, column + 1, m);
-
-            rowSafe[row][j] = false;
-            columnSafe[column][j] = false;
-            if (row == column)
-                m -= j;
-            square[row][column] = 0;
-        }
-    }
+	if(flag)
+		return;
+	if(j > n)
+		rec(i + 1, 1, n, sum);
+	if(i > n)
+	{
+		if(sum == 0)
+		{
+			cout << "POSSIBLE" << endl;
+			for(int i = 1; i <= n; i++)
+			{
+				for(int j = 1; j <= n; j++)
+					cout << a[i][j] << " ";
+				cout << endl;
+			}
+			flag = 1;
+		}
+		return;
+	}
+	vector<int> pos(n + 1, 1);
+	for(int k = 1; k < j; k++)
+		pos[a[i][k]] = 0;
+	for(int k = 1; k < i; k++)
+		pos[a[k][j]] = 0;
+	for(int val = 1; val <= n; val++)
+	{
+		if(!pos[val])
+			continue;
+		if(i == j && val > sum)
+			continue;
+		a[i][j] = val;
+		rec(i, j + 1, n, sum - ((i == j)?val:0));
+	}
 }
 
 int main()
 {
-    int t;
-    cin >> t;
-    for (i=0;i<t;i++)
-    {
-        scanf("%d %d",&n,&k);
-        evaluate(1, 1, 0);
-        if (!flag)
-            cout << "Case #" << (i+1) << ": " << "IMPOSSIBLE" << endl;
-        flag = false;
-    }
-    return 0;
+	IOS;
+	int t;
+	cin >> t;
+	int tc = 0;
+	while(t--)
+	{
+		tc++;
+		flag = 0;
+		int n, k;
+		cin >> n >> k;
+		cout << "Case #" << tc << ": ";
+		rec(1, 1, n, k);
+		if(!flag)
+			cout << "IMPOSSIBLE" << endl;
+	}
+	return 0;
 }
